8b209be672fd7c61d2d8377a5bcd59692fd6d41b
MazaFaaka <haitovvasyl@gmail.com>
2025-04-20 22:04:59 +0200

    finish profile task

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,35 @@
+import style from './Profile.module.css';
+
+export default function Profile({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) {
+  return (
+    <div className={style.container}>
+      <div className={style.box_profil}>
+        <img src={image} alt="User avatar" width={220} height={220} />
+        <p className={style.text}>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
